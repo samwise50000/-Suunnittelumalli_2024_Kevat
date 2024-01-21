ce3f3a84bf6a9719a808423ce91cfe8b0af164bf
samwise50000 <sami.mattila3@metropolia.fi>
2024-01-21 14:43:46 +0200

    Done.

--- Week1/Tehtava1/Main.java
+++ Week1/Tehtava1/Main.java
@@ -4,6 +4,16 @@ public class Main {
 
     public static void main(String[] args) {
         AterioivaOtus opettaja = new Opettaja();
+        AterioivaOtus luokkaA = new LuokanA();
+        AterioivaOtus luokkaB = new LuokanB();
+
+
         opettaja.aterioi();
+        System.out.println();
+
+        luokkaA.aterioi();
+        System.out.println();
+
+        luokkaB.aterioi();
     }
 }
